2d3c521c74a49de1294afecd6863c110739870c9
Alenka003 <98321432+Alenka003@users.noreply.github.com>
2022-01-24 15:23:27 +0300

    Add files via upload

---
+++ Laba 4/ConsoleApplication6.cpp
@@ -0,0 +1,43 @@
+﻿#include <fstream>
+#include <iostream>
+#include <Windows.h>
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const int len = 200;
+	char text[len];
+	char c[] = "ЁёУуАаОоЭэЯяИиЮю";
+	std::ifstream input;
+	std::ofstream output;
+	input.open("input.txt");
+	output.open("output.txt");
+
+	if (!(input.is_open() && output.is_open())) {
+		std::cout << "Не удалось открыть файл.";
+	}
+
+	// Чтение из файла
+	input.get(text, len, NULL);
+
+	int s = 0;
+
+	for (int i = 0; i < len; i++) {
+		if (text[i] == '\n' || text[i] == ' ' || text[i] == '\0') {
+			// Проверка на гласную букву
+			for (int j = 0; j < sizeof(c); j++) {
+				if (text[s] == c[j]) {
+					// Вывод слова
+					for (int k = s; k < i; k++) {
+						output << text[k];
+					}
+					output << '\n';
+					break; // Прервать проверку на гласную букву
+				}
+			}
+			// Сдвиг начала слова
+			s = ++i;
+		}
+		// Прервать глобальный цикл в конце строки
+		if (text[i] == '\0') break;
+	}
+}
